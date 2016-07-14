5b494875a6f0677d66000246d0fdb344b7e725b5
Igor Barbosa <igorbarbosa.p@gmail.com>
2016-07-14 17:02:29 -0300

    Remove db:test:clone from database template
    This task is deprecated in rails 5.
    The Rails test helper now maintains your test schema automatically.

--- lib/pah/templates/database.rb
+++ lib/pah/templates/database.rb
@@ -7,7 +7,7 @@ module Pah
         git add: 'config/database.yml'
         git_commit 'Add config/database.yml.'
 
-        run 'bundle exec rake db:create db:migrate db:test:clone'
+        run 'bundle exec rake db:create db:migrate'
 
         git add: 'db/schema.rb'
         git_commit 'Create database, adding db/schema.rb.'
